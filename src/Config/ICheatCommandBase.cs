049b339cd87b428b7219b7b70749f308bbaa375b
LSB52040 <105936304@qq.com>
2022-11-09 09:53:55 +0800

    Delete src directory

--- src/Config/ICheatCommandBase.cs
+++
@@ -1,8 +0,0 @@
-namespace CaptainOfCheats.Config
-{
-    public interface ICheatCommandBase
-    {
-        string Title { get; }
-        string Tooltip { get; set; }
-    }
-}
--- src/Constants/IconsPaths.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CaptainOfCheats.Constants
-{
-    public static class IconsPaths
-    {
-        public const string ToolbarCaptainWheel = "Assets/CaptainOfCheats/Toolbar/captain.png";
-    }
-}
